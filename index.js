fb5ab5a3427eed537c547ca641b5eb3e3b83a87e
Mikael Berg <miberg@silabs.com>
2014-10-18 01:32:42 +0200

    feat(core): add generators fromTheme, fromUrl, fromHtml and fromText
    Refactoring to an object based code, with generators returning a Codename object
    which can be made into a string with toString()

--- index.js
+++ index.js
@@ -6,14 +6,25 @@ var S = require('string');
 var extractor = require('unfluff');
 var Q = require('q');
 
-var URL_REGEX = /^http/i;
 var URL_WIKIPEDIA = 'http://en.wikipedia.org/wiki/<theme>';
 
-function getAll(words, classifier) {
+/**
+ * Initialize a new Codename
+ * @param {string} noun      First word in the codename
+ * @param {string} adjective Second word in the codename
+ */
+function Codename(adjective, noun) {
+    this.adjective = adjective;
+    this.noun = noun;
+}
+
+module.exports = Codename;
+
+function getWordType(words, classifier) {
     return words.filter(function(word) {
         return word[1].substr(0, classifier.length) === classifier;
     }).map(function(word) {
-        return word[0].toLowerCase();
+        return word[0];
     });
 }
 
@@ -21,34 +32,61 @@ function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
 
-function extractWords(html) {
-    var words = new pos.Lexer().lex(extractor(html).text);
-    return new pos.Tagger().tag(words);
-}
-
 function randomFromArray(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-function codename(theme) {
+Codename.prototype.toString = function() {
+    return S(this.adjective + ' ' + this.noun).slugify().s;
+};
+
+/**
+ * Generate a promise of a codename from a theme. Does a lookup on Wikipedia
+ * for the theme in order to find suitable words.
+ * @param {string} theme       Theme to find words from.
+ * @param {string} urlTemplate Optional template to use for the URL, where
+ *                             `<theme>` is replaced with the theme given.
+ * @return {promise} Promise of a Codename
+ */
+Codename.fromTheme = function(theme, urlTemplate) {
+    urlTemplate = urlTemplate ? urlTemplate : URL_WIKIPEDIA;
+    return Codename.fromUrl(urlTemplate.replace('<theme>', theme));
+};
+
+/**
+ * Generate a promise of a codename from an URL
+ * @param {string} url URL of a page to generate codename from.
+ * @return {promise} Promise of a Codename
+ */
+Codename.fromUrl = function(url) {
     var deferred = Q.defer();
 
-    theme = URL_REGEX.test(theme) ?
-            theme : URL_WIKIPEDIA.replace('<theme>', theme);
-
-    request(theme, function(error, response, body) {
-        var taggedWords = extractWords(body);
-
-        var adjectives = getAll(taggedWords, 'J').filter(onlyUnique);
-        var nouns = getAll(taggedWords, 'N').filter(onlyUnique);
-
-        var adjective = randomFromArray(adjectives);
-        var noun = randomFromArray(nouns);
-
-        deferred.resolve(S(adjective + ' ' + noun).slugify().s);
+    request(url, function(error, response, body) {
+        deferred.resolve(Codename.fromHtml(body));
     });
 
     return deferred.promise;
-}
+};
 
-module.exports = codename;
+/**
+ * Generate a codename from HTML
+ * @param {string} html The HTML to generate codename from.
+ * @return {Codename} Generated Codename
+ */
+Codename.fromHtml = function(html) {
+    return Codename.fromText(extractor(html).text);
+};
+
+/**
+ * Generate a codename from a body of text.
+ * @param {string} text Text to generate Codename from.
+ * @return {Codename} Generated Codename
+ */
+Codename.fromText = function(text) {
+    var words = new pos.Lexer().lex(text);
+    var taggedWords = new pos.Tagger().tag(words);
+    var adjectives = getWordType(taggedWords, 'J').filter(onlyUnique);
+    var nouns = getWordType(taggedWords, 'N').filter(onlyUnique);
+
+    return new Codename(randomFromArray(adjectives), randomFromArray(nouns));
+};
