c473a314db5d59ebc2bd894fb763414fedc8c61d
Mikael Berg <miberg@silabs.com>
2014-10-24 19:46:25 +0200

    feat(cli): add help shown with --help
    Show some help if --help is specified as a command line argument.

--- cli.js
+++ cli.js
@@ -2,12 +2,35 @@
 'use strict';
 
 var Codename = require('./');
+var multiline = require('multiline');
 var argv = require('minimist')(process.argv.slice(2));
 
-Codename.fromTheme(argv._[0] || 'Special:Random')
-    .then(function(name) {
-        console.log(name.toString());
-    })
-    .catch(function(error) {
-        console.error(error);
-    });
+function showHelp() {
+    console.log(multiline(function() {/*
+    Generate codenames e.g. for project releases easily. Make sure they stick to
+    a common theme of your choice.
+
+    Usage
+      codename
+      codename <theme>
+
+    Example
+      codename batman
+      codename artificial_intelligence
+
+    Arguments
+      theme     Name of a Wikipedia article from which to generate codenames.
+    */}));
+}
+
+if(argv.help) {
+    showHelp();
+} else {
+    Codename.fromTheme(argv._[0] || 'Special:Random')
+        .then(function(name) {
+            console.log(name.toString());
+        })
+        .catch(function(error) {
+            console.error(error);
+        });
+}
