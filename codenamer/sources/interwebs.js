236f04b1877e3102c5a8b40166aa4574c715fa13
Mikael Berg <mikael@zeppelin.no>
2016-02-02 20:38:36 +0100

    feat(sources): Let `interwebs` fetch from the internet

--- codenamer/sources/interwebs.js
+++ codenamer/sources/interwebs.js
@@ -2,6 +2,13 @@ const URLS = /^https?:\/\//i;
 
 function interwebs() {
   this.detect = (source) => URLS.test(source);
+  this.get = (source) => fetch(source)
+    .then(response => {
+      if (response.status >= 400) {
+        throw new Error(`Couldn't get ${source}`);
+      }
+      return response.text();
+    });
 }
 
 export default function create() {
--- test/sources/interwebs.test.js
+++ test/sources/interwebs.test.js
@@ -1,14 +1,32 @@
 import test from 'ava';
+import fs from 'fs';
+import fetchMock from 'fetch-mock';
 import interwebs from '../../codenamer/sources/interwebs';
 
+const FIXTURE = '../fixtures/batman.html';
+const MOCK_URL = 'http://mockurl';
+let source;
+
+test.beforeEach(() => source = interwebs());
+test.before(() => {
+  fetchMock.mock(MOCK_URL, () =>
+    fs.readFileSync(FIXTURE, { encoding: 'utf-8' }));
+});
+
 test('detects when it should', t => {
-  const source = interwebs();
+  t.ok(source.detect(MOCK_URL));
   t.ok(source.detect('http://en.wikipedia.org'));
   t.ok(source.detect('https://en.wikipedia.org'));
 });
 
 test('rejects when it should', t => {
-  const source = interwebs();
   t.notOk(source.detect('file.html'));
   t.notOk(source.detect('/file.html'));
+});
+
+test('it fetches the source url, returns string', async t => {
+  const result = await source.get(MOCK_URL);
+  t.is(typeof result, 'string');
+  t.ok(result.indexOf('batman') !== -1);
+  t.ok(result.length > 1000);
 });
