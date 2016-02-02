d9be51a14a083b3f83ab619dccae0b159ee25170
Mikael Berg <mikael@zeppelin.no>
2016-02-02 22:20:58 +0100

    feat(normalize): Create `normalize`, capable of normalizing HTML

---
+++ codenamer/normalize.js
@@ -0,0 +1,22 @@
+import unfluff from 'unfluff';
+import slug from 'slug';
+
+const SLUG_OPTIONS = {
+  lower: true,
+  remove: /[\d.]/g,
+};
+
+export default function normalize(source) {
+  let text = source;
+
+  if (/^\s*<!doctype/i.test(text)) {
+    text = unfluff(source.split('<').join(' <')).text;
+  }
+
+  text = text.split('/').join(' / ');
+
+  return text.split(' ')
+    .map(w => slug(w, SLUG_OPTIONS))
+    .map(w => w.split('-').join(''))
+    .filter(w => w.length > 0);
+}
