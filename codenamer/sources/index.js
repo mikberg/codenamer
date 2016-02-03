23fc13d3f806492ef1f48831a6c42a7a4c12da3a
Mikael Berg <mikael@zeppelin.no>
2016-02-03 20:55:32 +0100

    feat(sources): Export prioritized list of sources as default

--- codenamer/sources/index.js
+++ codenamer/sources/index.js
@@ -1,2 +1,11 @@
+import interwebs from './interwebs';
+import direct from './direct';
+
+// Defines prioritization of sources
+export default [
+  interwebs,
+  direct,
+];
+
 export interwebs from './interwebs';
 export direct from './direct';
