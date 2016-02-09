d0e3631dc3046ae4fa0d2f81b7aa92ee82c8fcdf
Mikael Berg <mikael@zeppelin.no>
2016-02-09 23:09:19 +0100

    feat(arrange): Add support for multiple words

--- codenamer/arrange.js
+++ codenamer/arrange.js
@@ -26,11 +26,33 @@ function createRelations(words, filters = []) {
     .filter(relation => relation.score > 0);
 }
 
+// relation -> [relation]
+function addWord(relation, words, filters = []) {
+  return words
+    .map(word => {
+      const combination = [...relation.words, word];
+      return {
+        words: combination,
+        score: scoreCombination(combination, filters),
+      };
+    })
+    .filter(rel => rel.score > 0);
+}
+
 export default function arrange(words, positions = []) {
   if (positions.length === 0) {
     return [];
   }
 
-  const relations = createRelations(words, positions[0]);
+  let relations = createRelations(words, positions[0]);
+
+  positions.slice(1).forEach(filters => {
+    let newRelations = [];
+    relations.forEach(relation => {
+      newRelations = [...newRelations, ...addWord(relation, words, filters)];
+    });
+    relations = newRelations;
+  });
+
   return relations.map(relation => relation.words);
 }
--- test/arrange.test.js
+++ test/arrange.test.js
@@ -4,7 +4,9 @@ import words from './fixtures/words';
 import { prefix } from '../codenamer/score';
 
 const onlyLength = length => wo => {
-  const total = wo.reduce((le, ri) => le.length + ri.length, '');
+  const total = wo
+    .map(w => w.length)
+    .reduce((le, ri) => le + ri, 0);
   return total === length ? 1 : 0;
 };
 
@@ -16,13 +18,25 @@ test('it can filter using a single filter on single words', t => {
   const filters = [[prefix('a')]];
   const results = arrange(words, filters);
   t.ok(results.length);
-  t.ok(results.filter(comb => !comb[0].startsWith('a')).length === 0);
+  t.ok(results.every(comb => comb[0].startsWith('a')));
 });
 
 test('it can use a series of filters for single words', t => {
   const filters = [[prefix('a'), onlyLength(4)]];
   const results = arrange(words, filters);
   t.ok(results.length);
-  t.ok(results.filter(comb => !comb[0].startsWith('a')).length === 0);
-  t.ok(results.filter(comb => comb[0].length !== 4).length === 0);
+  t.ok(results.every(comb => comb[0].startsWith('a')));
+  t.ok(results.every(comb => comb[0].length === 4));
+});
+
+test('it can create multi-word solutions', t => {
+  const filters = [
+    [prefix('a'), onlyLength(4)],
+    [prefix('a'), onlyLength(10)],
+  ];
+  const results = arrange(words, filters);
+  t.ok(results.length);
+  t.ok(results.every(comb => comb.length === 2));
+  t.ok(results.every(comb => comb[0].length === 4));
+  t.ok(results.every(comb => comb[1].length === 6));
 });
