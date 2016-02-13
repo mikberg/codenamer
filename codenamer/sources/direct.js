ffbbf6355f7c81ce7787455c8051a2d8db243e97
Mikael Berg <mikael@zeppelin.no>
2016-02-13 16:14:29 +0100

    refactor(grab): Remove `grab` and `source` modules

--- codenamer/sources/direct.js
+++
@@ -1,4 +0,0 @@
-export default {
-  detect: spec => typeof(spec) === 'string' && spec.split(' ').length > 1,
-  get: spec => Promise.resolve(spec),
-};
--- test/sources/index.test.js
+++
@@ -1,9 +0,0 @@
-import test from 'ava';
-import sources, * as sourcelibs from '../../codenamer/sources';
-
-test('sources is an array of all sources', t => {
-  t.ok(Array.isArray(sources));
-
-  t.plan(Object.keys(sourcelibs).length);
-  sources.forEach(source => t.ok(source.get && source.detect));
-});
