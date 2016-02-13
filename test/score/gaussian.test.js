37b6378dc61c69617c86680640d99d80a36ff847
Mikael Berg <mikael@zeppelin.no>
2016-02-13 14:49:39 +0100

    feat(score): Add gaussian length score

---
+++ test/score/gaussian.test.js
@@ -0,0 +1,9 @@
+import test from 'ava';
+import { gaussian } from '../../codenamer/score';
+
+test('it returns density with σ=10', t => {
+  const g = gaussian(10, 10);
+  t.ok(g(['abcdefghij']) - 0.126156626101 < 0.001);
+  t.ok(g(['abcde']) - 0.036144478534 < 0.001);
+  t.ok(g(['abcdefghijabcdefghij']) - 0.00085003666025 < 0.001);
+});
